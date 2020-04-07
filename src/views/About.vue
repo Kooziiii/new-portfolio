95ac4a010575b2acdb942cad6d7ddd762a180e94
小田　幸治 <swimmer0206@icloud.com>
2020-04-07 22:54:36 +0900

    [修正反映]

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,7 +4,7 @@
         <v-col
                 v-for="(item, i) in items"
                 :key="i"
-                cols="4"
+                :cols="item.flex"
                 class="mt-3 px-3"
         >
           <v-card
@@ -22,19 +22,23 @@
                         :inset="inset"
                         :color="item.bordercolor"
                 >
-
                 </v-divider>
+                <v-list-item :height="item.height" >
+                  <v-list-item-avatar
+                  >
+                    <v-img
+                            class="elevation-6"
+                            :src="item.src"
+                    ></v-img>
+                  </v-list-item-avatar>
+
+                </v-list-item>
                 <v-card-subtitle
                         class="black--text font-weight-bold"
                         v-text="item.text"
+
                 ></v-card-subtitle>
               </div>
-              <v-avatar
-                      class="ma-3"
-                      tile
-                      size="100"
-              >
-              </v-avatar>
             </div>
           </v-card>
         </v-col>
@@ -49,45 +53,58 @@
     data: () => ({
       items: [
         {
+          flex:'4',
+          height:'50px',
           bordercolor:'red',
           color: 'grey lighten-4',
           title: 'USER PROFILE',
           text: 'Foster the People',
+          src:'https://cdn.vuetifyjs.com/images/cards/mountain.jpg'
         },
         {
+          flex:'8',
+          height:'none',
           bordercolor:'deep-orange',
           color: 'grey lighten-4',
           title: 'INTRODUCTION',
-          text: 'こんにちは。小田幸治と申します。',
+          text: 'こんにちは。KOOOZIIIと申します。\n' +
+                  '信州大学経済学部を卒業後、ベンチャー企業にてフリーランスエンジニアへの\n' +
+                  '案件紹介エージェントとして勤務し、現在は前職の上司とともに、\n' +
+                  '同業種での会社の立ち上げを行っております。\n' +
+                  '仕事柄、エンジニアと触れ合うことが多く、その中で「自分の手でサービス\n' +
+                  'を作れるようになりたい」という想いが強くなり、エンジニアを志すように\n'+
+                  'なりました。\n'+
+                  'もともと大学時代からプログラミングの学習を継続しており、\n' +
+                  '3ヶ月間プログラミングスクールにも通っておりましたので、プログラミングの基礎の基礎の基礎は\n'+
+                  '理解できていると思います。\n' +
+                  'まだまだ駆け出しの身ではありますが、ご興味を持って頂けますと幸いです。\n' +
+                  'どうぞよろしくお願い致します！'
         },
         {
+          flex:'4',
+          height: '0',
           bordercolor:'green',
           color: 'grey lighten-4',
           title: 'QUALIFICATION',
           text: 'Ellie Goulding',
         },
         {
+          flex:'4',
+          height: '0',
           bordercolor:'blue',
           color: 'grey lighten-4',
           title: 'EXPERIENCE',
           text: 'Ellie Goulding',
         },
         {
+          flex: '4',
+          height: '0',
           bordercolor:'purple',
           color: 'grey lighten-4',
           title: 'HOBBY',
           text: 'Ellie Goulding',
-        },
-        {
-          bordercolor:'teal',
-          color: 'grey lighten-4',
-          title: 'FROM NOW ON',
-          text: 'これまで不動産や人材の業界に携わっており、' +
-                  'その中で様々な課題や問題・無駄があると' +
-                  '感じてきました。世の中に必要とされるサービスを' +
-                  '自分の手で作っていきたいと考えております。'
         }
-      ],
-    }),
+      ]
+    })
   }
 </script>
--- src/views/Outputs.vue
+++ src/views/Outputs.vue
@@ -23,13 +23,30 @@
                                     :inset="inset"
                                     class="grey lighten-1"
                             ></v-divider>
-                            <v-img
-                                    width="100%"
-                                    src="../assets/borderless-p.png"
-                                    height="200px"
-                                    class="align-end float-right mt-2 mb-5"
+                            <v-carousel
+                                    cycle
+                                    height="400"
+                                    hide-delimiter-background
+                                    show-arrows-on-hover
                             >
-                            </v-img>
+                                <v-carousel-item
+                                        v-for="(slide, i) in slides"
+                                        :key="i"
+                                >
+                                    <v-sheet
+                                            :color="colors[i]"
+                                            height="100%"
+                                    >
+                                        <v-row
+                                                class="fill-height"
+                                                align="center"
+                                                justify="center"
+                                        >
+                                            <div class="display-3">{{ slide }} Slide</div>
+                                        </v-row>
+                                    </v-sheet>
+                                </v-carousel-item>
+                            </v-carousel>
                             <v-card-subtitle
                                     class="black--text font-weight-bold pt-10 body"
                                     v-text="item.subtitle"
@@ -70,6 +87,20 @@
     export default {
         inset: false,
         data: () => ({
+            colors: [
+                'indigo',
+                'warning',
+                'pink darken-2',
+                'red lighten-1',
+                'deep-purple accent-4',
+            ],
+            slides: [
+                'First',
+                'Second',
+                'Third',
+                'Fourth',
+                'Fifth',
+            ],
             items: [
                 {
                     color: 'grey lighten-4',
