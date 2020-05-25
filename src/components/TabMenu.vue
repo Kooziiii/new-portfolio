dd7ee0697674dc60f49ed7288ea2540f3f0165e4
小田　幸治 <swimmer0206@icloud.com>
2020-05-26 07:52:34 +0900

    ヘッダータブ修正

--- src/components/TabMenu.vue
+++ src/components/TabMenu.vue
@@ -29,13 +29,11 @@
             <v-tabs class="hidden-sm-and-down">
                 <v-spacer></v-spacer>
                 <v-tab
-                        retain-focus-on-click="true"
                         color="white"
                         class="mt-4 mr-10
                         font-weight-bold"
                         elevation="0"
                         large
-                        :ripple="{ center: true }"
                         v-for="link in links" :key="link.text" router :to="link.route">
                         <v-icon left>{{link.icon}}</v-icon> {{link.text}}</v-tab>
             </v-tabs>
